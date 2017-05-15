41a120ca87efaab5a17dce6c523426b7c0b6904e
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-15 12:02:21 -0400

    creacion de nodos y asignacion de padres

--- js/main.js
+++ js/main.js
@@ -4,10 +4,32 @@ function agregaPosteo() {
 	var nombreFF = document.getElementById("nombre").value;
 	var contenidoFF = document.getElementById("cajaposteos").value;
 
+	//aca va a ir el post
 	var nuevoPost = document.createElement("div");
 
 	var contenedorNombre = document.createElement("strong");
 	var contenedorPost = document.createElement("p");
 	var separacion = document.createElement("hr");
+
+	//elemento para el corazon
+	var parrafCorazon = document.createElement('p');
+	var i = document.createElement('i');
+
+	parrafCorazon.appendChild(i);
+
+	parrafCorazon.setAttribute("class","corazon");
+	i.setAttribute("class","fa fa-heart");
+	i.setAttribute("arial-hidden","true");
+
+	var nodoNombre = document.createTextNode(nombreFF + " escribió: ");
+	var nodoPosteo = document.createTextNode(contenidoFF);
+
+	contenedorNombre.appendChild(nodoNombre);
+	contenedorPost.appendChild(nodoPosteo);
+
+	nuevoPost.appendChild(contenedorNombre);
+	nuevoPost.appendChild(contenedorPost);
+	nuevoPost.appendChild(parrafCorazon);
+	nuevoPost.appendChild(separacion);
 
 }
