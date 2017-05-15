7a93caeee56be20b68e2a48c414422e95cb8838d
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-15 11:49:14 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function agregaPosteo() {
-//escribe aca el codigo
+
+	var containerPosteos = document.getElementById("contenedor-posteos");
+	var nombreFF = document.getElementById("nombre").value;
+	var contenidoFF = document.getElementById("cajaposteos").value;
+
+	var nuevoPost = document.createElement("div");
+
+	var contenedorNombre = document.createElement("strong");
+	var contenedorPost = document.createElement("p");
+	var separacion = document.createElement("hr");
+
 }
